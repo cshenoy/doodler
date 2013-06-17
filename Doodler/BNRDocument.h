24d970f47e5954197fa5a18ea84b703138742d90
Chetan Shenoy <me@chetanshenoy.com>
2013-06-16 21:13:14 -0400

    Initial Commit

---
+++ Doodler/BNRDocument.h
@@ -0,0 +1,13 @@
+//
+//  BNRDocument.h
+//  Doodler
+//
+//  Created by Chetan Shenoy on 6/16/13.
+//  Copyright (c) 2013 Chetan Shenoy. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface BNRDocument : NSDocument
+
+@end
---
+++ Doodler/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  Doodler
+//
+//  Created by Chetan Shenoy on 6/16/13.
+//  Copyright (c) 2013 Chetan Shenoy. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
