84ecd7ebe406e5f85b42f0151536b513a08a724e
Chetan Shenoy <me@chetanshenoy.com>
2013-06-20 12:27:34 -0400

    create/delete methods added. to-do basic functionality is all there now.

--- Doodler/BNRDocument.m
+++ Doodler/BNRDocument.m
@@ -28,6 +28,73 @@
     return @"BNRDocument";
 }
 
+#pragma mark - Actions
+
+- (IBAction)createNewItem:(id)sender
+{
+    // If there's no array yet, go ahead and create one to store our new task
+    if (!todoItems){
+        todoItems = [NSMutableArray array];
+    }
+    
+    [todoItems addObject:@"New Item"];
+    
+    // -reloadData tells the table view to refresh and ask its dataSource
+    // (which happens to be this BNRDocument object in this case)
+    // for new data to display
+    [itemTableView reloadData];
+    
+    // -updateChangeCount: tells the application whether or not the document
+    // has unsaved changes. NSChangeDone flags the document as unsaved.
+    [self updateChangeCount:NSChangeDone];
+}
+
+- (IBAction)deleteItem:(id)sender
+{
+    NSInteger blah = [itemTableView selectedRow];
+    
+    NSLog(@"blah blah %ld", blah);
+    if (!todoItems){
+        return;
+    }
+    [todoItems removeObjectAtIndex:blah];
+    
+    [itemTableView reloadData];
+    
+    [self updateChangeCount:NSChangeDone];
+}
+
+#pragma mark - Data Source Methods
+
+- (NSInteger)numberOfRowsInTableView:(NSTableView *)tableView
+{
+    // This table view is meant to display the todoItems,
+    // so the number of entries in the table view will be the same
+    // as the number of objects in the array.
+    return [todoItems count];
+}
+
+- (id)tableView:(NSTableView *)tableView
+objectValueForTableColumn:(NSTableColumn *)tableColumn
+            row:(NSInteger)row
+{
+    // Return the item from todoItems that corresponds to the cell
+    // that the table view wants to display
+    return [todoItems objectAtIndex:row];
+}
+
+- (void)tableView:(NSTableView *)tableView
+   setObjectValue:(id)object
+   forTableColumn:(NSTableColumn *)tableColumn
+              row:(NSInteger)row
+{
+    // When the user changes a to-do item on the table view,
+    // update the todoItems array
+    [todoItems replaceObjectAtIndex:row withObject:object];
+    // And then flag the document as having unsaved changes.
+    [self updateChangeCount:NSChangeDone];
+}
+
 - (void)windowControllerDidLoadNib:(NSWindowController *)aController
 {
     [super windowControllerDidLoadNib:aController];
@@ -39,23 +106,44 @@
     return YES;
 }
 
-- (NSData *)dataOfType:(NSString *)typeName error:(NSError **)outError
+- (NSData *)dataOfType:(NSString *)typeName
+                 error:(NSError **)outError
 {
-    // Insert code here to write your document to data of the specified type. If outError != NULL, ensure that you create and set an appropriate error when returning nil.
-    // You can also choose to override -fileWrapperOfType:error:, -writeToURL:ofType:error:, or -writeToURL:ofType:forSaveOperation:originalContentsURL:error: instead.
-    NSException *exception = [NSException exceptionWithName:@"UnimplementedMethod" reason:[NSString stringWithFormat:@"%@ is unimplemented", NSStringFromSelector(_cmd)] userInfo:nil];
-    @throw exception;
-    return nil;
+    // This method is called when our document is being saved
+    // We are expected to hand the caller an NSData object wrapping
+    // so that it can be written to disk
+    
+    // If there's no array, we'll write out an empty array for now
+    if(!todoItems) {
+        todoItems = [NSMutableArray array];
+    }
+    
+    // Pack our todoItems array into an NSData Object
+    NSData *data = [NSPropertyListSerialization dataWithPropertyList:todoItems
+                                                              format:NSPropertyListXMLFormat_v1_0
+                                                             options:0
+                                                               error:outError];
+    
+    // return our newly-packed NSData object
+    return data;
 }
 
-- (BOOL)readFromData:(NSData *)data ofType:(NSString *)typeName error:(NSError **)outError
+- (BOOL)readFromData:(NSData *)data
+              ofType:(NSString *)typeName
+               error:(NSError **)outError
 {
-    // Insert code here to read your document from the given data of the specified type. If outError != NULL, ensure that you create and set an appropriate error when returning NO.
-    // You can also choose to override -readFromFileWrapper:ofType:error: or -readFromURL:ofType:error: instead.
-    // If you override either of these, you should also override -isEntireFileLoaded to return NO if the contents are lazily loaded.
-    NSException *exception = [NSException exceptionWithName:@"UnimplementedMethod" reason:[NSString stringWithFormat:@"%@ is unimplemented", NSStringFromSelector(_cmd)] userInfo:nil];
-    @throw exception;
-    return YES;
+    // This method is called when a document is being loaded
+    // We are handed an NSData object and expected to pull our data
+    
+    // Extract our todoItems
+    todoItems = [NSPropertyListSerialization
+                 propertyListWithData:data
+                 options:NSPropertyListMutableContainers
+                 format:NULL
+                 error:outError];
+    
+    // return success or failure depending on success of the above call
+    return (todoItems != nil);
 }
 
 @end
